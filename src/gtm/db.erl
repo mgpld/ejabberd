bb6499c7ec49fe5c9e5fa3c02dec786eeb1e7c88
Antoine <ak@harmonygroup.net>
2016-02-07 23:20:19 +0100

    Database asynchronous call (cast)

--- src/gtm/db.erl
+++ src/gtm/db.erl
@@ -1,13 +1,14 @@
 -module(db).
 % Created db.erl the 13:26:07 (02/05/2014) on core
-% Last Modification of db.erl at 20:07:26 (09/07/2014) on core
+% Last Modification of db.erl at 23:20:15 (07/02/2016) on sd-19230
 % 
 % Author: "rolph" <rolphin@free.fr>
 
 % Simple wrapper to hide the 'gtmproxy' process
 
 -export([
-    call/3, call/4
+    call/3, call/4,
+    cast/4
 ]).
 
 call(Method, Module, Args) ->
@@ -20,3 +21,7 @@ call(Method, Module, Args, undefined ) ->
 call(Method, Module, Args, Timeout) ->
     Call = [ Method, Module, Args ],
     catch gen_server:call( gtmproxy, {call, Call}, Timeout).
+
+cast(TransId, Method, Module, Args) ->
+    catch gen_server:cast(gtmproxy, {self(), [TransId, Method, Module, Args]}).
+
