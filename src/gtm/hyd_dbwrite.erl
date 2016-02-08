bd0e4e258bc4c91fb56ec78fa3ccec7ac5546383
Antoine <ak@harmonygroup.net>
2016-02-08 11:32:03 +0100

    Log enhancement and removed unused code

--- src/gtm/hyd_dbwrite.erl
+++ src/gtm/hyd_dbwrite.erl
@@ -1,6 +1,6 @@
 -module(hyd_dbwrite).
 % Created hyd_dbwrite.erl the 12:25:51 (26/01/2016) on core
-% Last Modification of hyd_dbwrite.erl at 14:08:07 (26/01/2016) on core
+% Last Modification of hyd_dbwrite.erl at 11:31:53 (08/02/2016) on sd-19230
 % 
 % Author: "rolph" <ak@harmonygroup.net>
 
@@ -24,13 +24,9 @@
 %%%===================================================================
 tcp_init(_Socket, _Opts) ->
     ok.
-    %ejabberd:start_app(p1_stun),
-    %stun:tcp_init(Socket, prepare_turn_opts(Opts)).
 
 udp_init(_Socket, _Opts) ->
     ok.
-    %ejabberd:start_app(p1_stun),
-    %stun:udp_init(Socket, prepare_turn_opts(Opts)).
 
 udp_recv(Socket, Addr, Port, Packet, Opts) ->
     spawn(fun() ->
@@ -46,51 +42,6 @@ socket_type() ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-prepare_turn_opts(Opts) ->
-    UseTurn = proplists:get_bool(use_turn, Opts),
-    prepare_turn_opts(Opts, UseTurn).
-
-prepare_turn_opts(Opts, _UseTurn = false) ->
-    Opts;
-prepare_turn_opts(Opts, _UseTurn = true) ->
-    NumberOfMyHosts = length(?MYHOSTS),
-    case proplists:get_value(turn_ip, Opts) of
-	undefined ->
-	    ?WARNING_MSG("option 'turn_ip' is undefined, "
-			 "more likely the TURN relay won't be working "
-			 "properly", []);
-	_ ->
-	    ok
-    end,
-    AuthFun = fun ejabberd_auth:get_password_s/2,
-    Shaper = gen_mod:get_opt(shaper, Opts,
-			     fun(S) when is_atom(S) -> S end,
-			     none),
-    AuthType = gen_mod:get_opt(auth_type, Opts,
-			       fun(anonymous) -> anonymous;
-				  (user) -> user
-			       end, user),
-    Realm = case gen_mod:get_opt(auth_realm, Opts, fun iolist_to_binary/1) of
-		undefined when AuthType == user ->
-		    if NumberOfMyHosts > 1 ->
-			    ?WARNING_MSG("you have several virtual "
-					 "hosts configured, but option "
-					 "'auth_realm' is undefined and "
-					 "'auth_type' is set to 'user', "
-					 "more likely the TURN relay won't "
-					 "be working properly. Using ~s as "
-					 "a fallback", [?MYNAME]);
-		       true ->
-			    ok
-		    end,
-		    [{auth_realm, ?MYNAME}];
-		_ ->
-		    []
-	    end,
-    MaxRate = shaper:get_max_rate(Shaper),
-    Realm ++ [{auth_fun, AuthFun},{shaper, MaxRate} |
-	      lists:keydelete(shaper, 1, Opts)].
-
 handle_packet(Socket, Addr, Port, Packet, Opts) ->
     ?DEBUG(?MODULE_STRING ".(~p) Packet: ~p, Opts: ~p", [ ?LINE, Packet, Opts ]),
     [ Head | Args ] = binary:split(Packet, <<" ">>),
@@ -103,16 +54,15 @@ handle_packet(Socket, Addr, Port, Packet, Opts) ->
             ok;
 
         {db, TransId, {error, Error}} ->
-            ?DEBUG(?MODULE_STRING ".~p FAIL! Domain: ~p Method: ~p, Args: ~p: ~p", [ ?LINE, Domain, Method, Args, Error ]),
+            ?ERROR_MSG(?MODULE_STRING ".~p FAIL! Domain: ~p Method: ~p, Args: ~p: ~p", [ ?LINE, Domain, Method, Args, Error ]),
             ok;
     
         _Result ->
-            ?DEBUG(?MODULE_STRING ".~p FAIL! Domain: ~p Method: ~p, Args: ~p: ~p", [ ?LINE, Domain, Method, Args, _Result ]),
+            ?ERROR_MSG(?MODULE_STRING ".~p FAIL! Domain: ~p Method: ~p, Args: ~p: ~p", [ ?LINE, Domain, Method, Args, _Result ]),
             ok
             
     after 1000 ->
-            ?DEBUG(?MODULE_STRING ".~p FAIL! Timeout Method: ~p, Args: ~p", [ ?LINE, Method, Args ]),
-            %handle_packet(Socket, Addr, Port, Packet, Opts)
+            ?DEBUG(?MODULE_STRING ".~p FAIL! Timeout: Domain: ~p Method: ~p, Args: ~p", [ ?LINE, Domain, Method, Args ]),
             ok
     end.
     
