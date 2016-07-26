3969f9578026a8f64255012761c3b4a7d2a81c30
Mgpld <mgpld@free.fr>
2016-07-26 21:52:32 +0000

    Addded utf8 utils

---
+++ src/gtm/utf8_utils.erl
@@ -0,0 +1,17 @@
+-module(utf8_utils).
+% Created utf8_utils.erl the 20:31:29 (11/07/2016) on core
+% Last Modification of utf8_utils.erl at 20:52:39 (11/07/2016) on core
+% 
+% Author: "ak" <ak@harmonygroup.net>
+
+-export([
+    count/1, count/2
+]).
+
+count(Bin) ->
+    count(Bin, 0).
+
+count(<<>>, Count) ->
+    Count;
+count(<<_/utf8,Rest/binary>>, Count) ->
+    count(Rest, Count + 1).
