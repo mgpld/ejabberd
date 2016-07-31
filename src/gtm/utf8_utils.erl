6257a11d07ab408404376c4602dec458db8c16d8
Mgpld <mgpld@free.fr>
2016-07-31 11:32:52 +0000

    Optimize UTF8 count

--- src/gtm/utf8_utils.erl
+++ src/gtm/utf8_utils.erl
@@ -1,6 +1,6 @@
 -module(utf8_utils).
 % Created utf8_utils.erl the 20:31:29 (11/07/2016) on core
-% Last Modification of utf8_utils.erl at 20:52:39 (11/07/2016) on core
+% Last Modification of utf8_utils.erl at 11:32:40 (31/07/2016) on core
 % 
 % Author: "ak" <ak@harmonygroup.net>
 
@@ -13,5 +13,11 @@ count(Bin) ->
 
 count(<<>>, Count) ->
     Count;
+count(<<_/utf8,_/utf8,_/utf8,Rest/binary>>, Count) ->
+    count(Rest, Count + 3);
+count(<<_/utf8,_/utf8,Rest/binary>>, Count) ->
+    count(Rest, Count + 2);
 count(<<_/utf8,Rest/binary>>, Count) ->
+    count(Rest, Count + 1);
+count(<<_,Rest/binary>>, Count) ->
     count(Rest, Count + 1).
